410c90517a04d9df2a2b24755da43e32633abe4b
Polprzewodnikowy <sc@mateuszfaderewski.pl>
2020-10-24 22:49:51 +0200

    fixeee

--- build.sh
+++ build.sh
@@ -12,17 +12,17 @@ docker pull anacierdem/libdragon:latest
 echo "Building bootloader"
 docker run -t --mount src="$(pwd)/sw/bootloader",target="/libdragon",type=bind anacierdem/libdragon:latest /usr/bin/make N64_BYTE_SWAP=false
 
+# Build UltraCIC-III
+pushd sw/cic
+echo "Building UltraCIC-III"
+avra UltraCIC-III.asm -D attiny45
+popd
+
 # Build FPGA firmware
 echo "Pulling Quartus Lite"
 docker pull chriz2600/quartus-lite:20.1.0
 echo "Building FPGA firmware"
-docker run -t --mount src="$(pwd)",target="/build",type=bind chriz2600/quartus-lite quartus_wrapper quartus_sh --flow compile /build/fw/SummerCart64.qpf
-
-# Build UltraCIC-III
-pushd UltraCIC-III
-echo "Building UltraCIC-III"
-avra UltraCIC-III.asm -D attiny45
-popd
+docker run -t --mount src="$(pwd)",target="/build",type=bind chriz2600/quartus-lite:20.1.0 quartus_wrapper quartus_sh --flow compile /build/fw/SummerCart64.qpf
 
 # ZIP files for release
 zip -j ${PACKAGE_FILE_NAME}.zip \
@@ -31,4 +31,4 @@ zip -j ${PACKAGE_FILE_NAME}.zip \
     ./fw/output_files/SummerCart64_pof.svf \
     ./fw/output_files/SummerCart64.svf \
     ./hw/ftdi-template-release.xml \
-    ./UltraCIC-III/UltraCIC-III.hex
+    ./sw/cic/UltraCIC-III.hex
