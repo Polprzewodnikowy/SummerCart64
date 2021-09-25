e42c8ce66034e918d08bb415f407fc09c324743e
Polprzewodnikowy <sc@mateuszfaderewski.pl>
2021-09-25 22:14:52 +0200

    test 2

--- sw/n64/build.sh
+++ sw/n64/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --mount type=bind,src="$(pwd)",target="/workdir" polprzewodnikowy/sc64env:0.9 /bin/bash -c "make clean all"
+docker run --mount type=bind,src="$(pwd)",target="/workdir" ghcr.io/polprzewodnikowy/sc64env /bin/bash -c "make clean all"
