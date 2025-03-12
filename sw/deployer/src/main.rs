b88c9a314b697605c7b74454d4a8d47e7d58ff92
Mateusz Faderewski <sc@mateuszfaderewski.pl>
2025-03-12 23:05:13 +0100

    [SC64][SW] Added license notice in the sc64deployer help

--- sw/deployer/src/main.rs
+++ sw/deployer/src/main.rs
@@ -21,7 +21,36 @@ use std::{
 };
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    author,
+    version,
+    about,
+    help_template = "\
+{before-help}{name} v{version} - {about}
+Copyright (C) 2020 - 2025 {author}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+Latest information about SummerCart64 is available
+on the official website: <https://summercart64.dev/>.
+
+
+{usage-heading} {usage}
+
+{all-args}{after-help}"
+)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
